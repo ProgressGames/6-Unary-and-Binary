5a05581ae4296056ab5ae5a998da97ec5f06dbc2
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-01 21:21:26 +0300

    Add

---
+++ Binary.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//БИНАРНЫЕ | Binary
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	int A = 5, B = 2, c;
+	c = A + B;
+		cout << c << endl;
+}
---
+++ Unary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+//УНАРНЫЕ | Unary
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	int A = 5;
+        cout << A << endl;
+        A = -A; // -A делает 5 в -5 | -A does 5 in -5
+        cout << A << endl;
+}
